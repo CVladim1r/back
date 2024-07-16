dca99e4bd3c37a2264248b5604d5a26ccc9eaa17
CVladim1r <vladimir.973@list.ru>
2024-07-17 02:43:09 +0300

    mini fix

--- back/main.py
+++ back/main.py
@@ -5,15 +5,19 @@ from routers import api, ws
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "ws://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Add more methods as needed
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
-# WebSocket CORS middleware
 class WebSocketCORSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
@@ -23,11 +27,9 @@ class WebSocketCORSMiddleware(BaseHTTPMiddleware):
 
 app.add_middleware(WebSocketCORSMiddleware)
 
-# Include your API and WebSocket routers
 app.include_router(api.router, prefix="/api")
 app.include_router(ws.router, prefix="/ws")
 
-# Example route
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
--- testfront/src/pages/Home.tsx
+++ testfront/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-// src/pages/Home.tsx
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -7,14 +6,19 @@ const Home = () => {
   const navigate = useNavigate();
 
   const handleJoinRoom = () => {
-    const playerId = Date.now().toString(); // Уникальный идентификатор игрока
+    const playerId = Date.now().toString();
     navigate(`/room/${roomId}/${playerId}`);
   };
 
   return (
     <div>
       <h1>Welcome to Durak Game</h1>
-      <input type="text" value={roomId} onChange={(e) => setRoomId(e.target.value)} placeholder="Enter Room ID" />
+      <input
+        type="text"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+        placeholder="Enter Room ID"
+      />
       <button onClick={handleJoinRoom}>Join Room</button>
     </div>
   );
