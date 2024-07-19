f48310ff9be389207b7b8dcaef27d78f699d55bb
CVladim1r <vladimir.973@list.ru>
2024-07-19 12:07:42 +0300

    gs

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-    host: '0.0.0.0', // Позволяет доступ с других устройств в сети
-  },
-})
+});
